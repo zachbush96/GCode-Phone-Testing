835a5da19b630f3bb7ac23c8e38e0d39aff1fedd
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2022-11-01 18:49:36 -0500

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -3,38 +3,59 @@ import time
 import cv2 as cv
 import numpy as np
 import ObjectBasedGCode
+import os
 
 #camera = cv.VideoCapture(0) #Camera 1
 #camera = cv.VideoCapture(1) #Camera 2
 
-
+#A function that returns an array containing the number of cameras attached to the computer
+def returnCameraIndexes():
+    # checks the first 10 indexes.
+    index = 0
+    arr = []
+    i = 10
+    while i > 0:
+        cap = cv.VideoCapture(index)
+        if cap.read()[0]:
+            arr.append(index)
+            cap.release()
+        index += 1
+        i -= 1
+    return arr
 
 
 def capturePicture(name, cncDeviceProfile):
-    camera = cv.VideoCapture(int(cncDeviceProfile.camera))
-    camera.set(3, 3840)
+    dirname = "IMAGES/"+cncDeviceProfile.imagePath
+    if not os.path.exists(dirname): #If the folder does not exist, create it
+        os.makedirs(dirname)
+    
+    try:
+        camera = cv.VideoCapture(int(cncDeviceProfile.camera)) 
+    except:
+        print("Error: Check the camera number in the CNC Device Profile")
+    camera.set(3, 3840) #Images are captured in 4K
     camera.set(4, 2160)
-    time.sleep(1)
     ret, frame = camera.read()
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...\n")
         return
     #Save image to folder called IMAGES with the name of the parameter
-    cv.imwrite("/IMAGES/" + cncDeviceProfile.imagePath +  name + ".jpg", frame)
-    print(f'\nImage Taken: {name}\n')
+    cv.imwrite(os.path.join(dirname, name + '.jpg'), frame)
+    if ObjectBasedGCode.TROUBLESHOOTING:
+        print(f'\nImage Taken: {name}\n')
+        print(f'File Path: {cncDeviceProfile.imagePath + name + ".jpg"}\n')
     camera.release()
     cv.destroyAllWindows()
     if name == "Sensor Information":
-        #Do OCR on the image
         pass
 
-
+#Function that is mainly used for troubleshooting what the camera see's
 def showCameraView(cncDeviceProfile):
     camera = cv.VideoCapture(int(cncDeviceProfile.camera))
     camera.set(3, 1280)
     camera.set(4, 720)
-    time.sleep(5)
+    time.sleep(1.5)
     while True:
         ret, frame = camera.read()
         cv.imshow(f'Camera', frame)
@@ -46,7 +67,7 @@ def showCameraView(cncDeviceProfile):
 
 
 def showAllConnectedCameras():
-    #Ask user how many cameras are connected
+    print(f'I have deteched {len(returnCameraIndexes())} cameras connected to this computer: {returnCameraIndexes()}')
     cameraCount = int(input("How many cameras are connected? "))
     #For each camera, show a stream of the camera with a number on top as to which option it is
     for i in range(cameraCount):
