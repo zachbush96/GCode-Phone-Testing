936c16ebcc567636fef764e592496f124bd733a6
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2022-11-18 16:50:58 -0600

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -5,9 +5,11 @@ import ObjectBasedGCode
 import os
 import easyocr
 import requests
+import imutils
+import numpy as np
+import glob
 
-#camera = cv.VideoCapture(0) #Camera 1
-#camera = cv.VideoCapture(1) #Camera 2
+
 
 #A function that returns an array containing the number of cameras attached to the computer
 def returnCameraIndexes():
@@ -24,7 +26,6 @@ def returnCameraIndexes():
         i -= 1
     return arr
 
-
 def capturePicture(name, cncDeviceProfile):
     dirname = "IMAGES/"+cncDeviceProfile.imagePath
     if not os.path.exists(dirname): #If the folder does not exist, create it
@@ -33,24 +34,23 @@ def capturePicture(name, cncDeviceProfile):
         camera = cv.VideoCapture(int(cncDeviceProfile.camera)) 
     except:
         print("Error: Check the camera number in the CNC Device Profile")
-    camera.set(3, 3840) #Images are captured in 4K
+    camera.set(3, 3840)
     camera.set(4, 2160)
     ret, frame = camera.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...\n")
         return
-    #Rotate the image -90 degrees
-    frame = cv.rotate(frame, cv.ROTATE_90_COUNTERCLOCKWISE)
+    #Rotate the image 90 degrees
+    frame = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE)
     #Save image to folder called IMAGES with the name of the parameter
     cv.imwrite(os.path.join(dirname, name + '.jpg'), frame)
     if ObjectBasedGCode.TROUBLESHOOTING:
         print(f'\nImage Taken: {name}\n')
         print(f'File Path: {cncDeviceProfile.imagePath +"/"+ name + ".jpg"}\n')
     camera.release()
-    #cv.destroyAllWindows() #Is this not needed?
-    if name == "Sensor Information":
-        pass
-
+    #Return the file path to the new image
+    return cncDeviceProfile.imagePath +"/"+ name + ".jpg"
+    
 #Function that is mainly used for troubleshooting what the camera see's
 def showCameraView(cncDeviceProfile):
     camera = cv.VideoCapture(int(cncDeviceProfile.camera))
@@ -66,7 +66,6 @@ def showCameraView(cncDeviceProfile):
             break
     camera.release()
     cv.destroyAllWindows()
-
 
 def showAllConnectedCameras():
     print(f'I have deteched {len(returnCameraIndexes())} cameras connected to this computer: {returnCameraIndexes()}')
@@ -103,7 +102,6 @@ def findObjectOutline(imagePath):
     cv.destroyAllWindows()
     return contours[0]
 
-
 #A function that sends an image to a OCR API via a post request and returns the text
 def readText(imagePath):
     API_IP = "http://127.0.0.1:5000/ocr"
@@ -114,24 +112,101 @@ def readText(imagePath):
         finalElements.append(element.split("'")[1])
     return finalElements
 
-
-
-#A function that accepts an image path and returns if the image of a phone has screen burn in
+#A function that accepts an image path, finds the edges of the object, and returns the areas of the object that fall outside a specific color range
 def checkForScreenBurnIn(imagePath):
     img = cv.imread(imagePath)
-    img = cv.resize(img, (1280, 720))
+    #Find the object outline
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(gray, 127, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(img, contours, -1, (0, 255, 0), 3)
-    cv.imshow('img', img)
+    #Find the edges of the object
+    edges = cv.Canny(img, 100, 200)
+    #Show the edges
+    cv.imshow('edges', edges)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    return contours[0]
 
 
+def testImages(imagePath): #For the SubKey test, cnc argument is to get the image directory
+    imagePath = "IMAGES/"+imagePath
+    testResult = "FAIL" #Default to FAIL, but if all the images are good, set to PASS
+    individualTestResults = []
+    print("Image File: " + imagePath)
+    if "VolumeUp" in imagePath:
+        exptectedColor = "White"
+    elif "VolumeDown" in imagePath:
+        exptectedColor = "Green"
+    elif "Power" in imagePath:
+        exptectedColor = "Red"
+    elif "Back" in imagePath:
+        exptectedColor = "Magenta"
+    elif "Home" in imagePath:
+        exptectedColor = "Blue"
+    elif "Menu" in imagePath:
+        exptectedColor = "Orange"
+    print("Expected Color: " + exptectedColor)
+    img = cv.imread(imagePath)
+    img = cv.resize(img, (1, 1))
+    color = img[0, 0]
+    color = str(color)
+    print("Color: " + color)
+    color = color.replace("[", "")
+    color = color.replace("]", "")
+    blueValue = int(color[0]+color[1]+color[2])
+    greenValue = int(color[4]+color[5]+color[6])
+    redValue = int(color[8]+color[9]+color[10])
+    print(f'Red: {redValue} Green: {greenValue} Blue: {blueValue}')  
+    if exptectedColor == "White":
+        if redValue > 200 and greenValue > 200 and blueValue > 200:
+            testResult = "PASS"
+            individualTestResults.append(testResult)
+        else:
+            individualTestResults.append("FAIL")
+    
+    elif exptectedColor == "Green":
+        if redValue < 100 and greenValue > 100 and blueValue < 150:
+            testResult = "PASS"
+            individualTestResults.append(testResult)
+        else:
+            individualTestResults.append("FAIL")
+    
+    elif exptectedColor == "Red":
+        if redValue > 200 and greenValue < 150 and blueValue < 150:
+            testResult = "PASS"
+            individualTestResults.append(testResult)
+        else:
+            individualTestResults.append("FAIL")
+    
+    elif exptectedColor == "Magenta":
+        if redValue > 100 and greenValue < 100 and blueValue > 100:
+            testResult = "PASS"
+            individualTestResults.append(testResult)
+        else:
+            individualTestResults.append("FAIL") 
+    
+    elif exptectedColor == "Blue":
+        if redValue < 100 and greenValue < 100 and blueValue > 100:
+            testResult = "PASS"
+            individualTestResults.append(testResult)
+        else:
+            individualTestResults.append("FAIL")
+    
+    elif exptectedColor == "Orange":
+        if redValue > 100 and greenValue > 100 and blueValue < 100:
+            testResult = "PASS"
+            individualTestResults.append(testResult)
+        else:
+            individualTestResults.append("FAIL")
+    
+    return testResult
 
 if __name__ == "__main__":
     #print(readText("IMAGES/rotatedImages/Sensor143.jpg"))
-    #print(checkForScreenBurnIn("IMAGES/path1114/GoodBlue59528.jpg"))
+    #print(checkForScreenBurnIn("IMAGES/imagePath/red.jpg"))
+    #print(testImages("IMAGES/subKeyOnly/SubKey_VolumeDown95643.jpg"))
+    #For image in IMAGES/subKeyOnly that starts with SubKey, run the testImages function
+    for image in glob.glob("IMAGES/subKeyOnly/SubKey*.jpg"):
+        print(testImages(image))
+        print("\n")
     pass
