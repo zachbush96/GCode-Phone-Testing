1c1d011c6812450f005f52bd64b5dbb11ddd3602
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2022-11-18 16:51:46 -0600

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -7,37 +7,68 @@ import tkinter as tk
 import time
 
 #Frequently Used Variables
-FSPEED = 3000
-x = 5
-y = 5
-x_length = 600
-y_length = 360
+FSPEED = 4500
+CNC_x = 5
+CNC_y = 5
+x_length = 600 #(300 * 2)
+y_length = 360 #(180 * 2)
 
-#Setup tkinter
+#Create a Tkinter windows using the x_length and y_length variables as the size of the window with a black background and a white circle that represents the CNC machine
 root = tk.Tk()
 root.title("CNC Machine Position")
-root.geometry("600x360")
-root.configure(background='black')
-canvas = tk.Canvas(root, width=x_length, height=y_length, bg="black")
-CNC = canvas.create_oval(x, y, x+10, y+10, fill="white")
-
-
-#Add a ruler to the top of the tkinter windows from 0 to 300mm
-for i in range(0, 600, 50):
-    canvas.create_line(i, 5, i, 1, fill="white")
-    canvas.create_text(i, 7, text=str(i), fill="white")
-
-#Add a ruler to the left side of the tkinter window from 0 to 180mm
-for i in range(0, 360, 50):
-    canvas.create_line(7, i, 1, i, fill="white")
-    canvas.create_text(9, i, text=str(i), fill="white")
-
-#Move the CNC oval to a new position ( 100 , 100)
-def moveCNC(x, y):
-    canvas.move(CNC, x, y)
-    root.update()
-
+root.geometry(f'{x_length}x{y_length}')
+root.configure(bg='black')
+canvas = tk.Canvas(root, width=x_length, height=y_length, bg='black')
 canvas.pack()
-moveCNC(150, 150)
-canvas.pack()
+canvas.create_oval(CNC_x, CNC_y, 20, 20, fill='white')
+#Add a listener for when i click the escape key to close the Tkinter window
+root.bind('<Escape>', lambda e: root.destroy())
+
+#Add a ruler to the top of the Tkinter window from 0 to 300mm with a 25mm interval
+for i in range(0, 301, 25):
+    canvas.create_line(i*2, 0, i*2, 10, fill='white')
+    canvas.create_text(i*2, 15, text=i, fill='white', font=('Arial', 8))
+#Add a ruler to the left side of the Tkinter window from 0 to 180mm with a 25mm interval
+for i in range(0, 181, 25):
+    canvas.create_line(0, i*2, 10, i*2, fill='white')
+    canvas.create_text(15, i*2, text=i, fill='white', font=('Arial', 8))
+
+#Create a function that moves the CNC machine oval to a specific location over a period of time (time is calculated using the distance between the current location and the new location and the speed of the CNC machine)
+def move(x, y):
+    global CNC_x
+    global CNC_y
+    global canvas
+    global root
+    global FSPEED
+    
+    currentPosition = {
+        'x': CNC_x,
+        'y': CNC_y
+    }
+    
+    #Calculate the distance between the current location and the new location
+    distance = ((x-CNC_x)**2 + (y-CNC_y)**2)**0.5
+    #Calculate the time it will take to move from the current location to the new location
+    time_to_move = distance / (FSPEED/60)
+    #Calculate the number of steps it will take to move from the current location to the new location
+    steps = int(time_to_move * 100)
+    #Calculate the distance between each step
+    x_step = (x-CNC_x)/steps
+    y_step = (y-CNC_y)/steps
+    #Move the CNC machine oval to the new location over a period of time
+    for i in range(steps):
+        CNC_x += x_step
+        CNC_y += y_step
+        canvas.coords(1, CNC_x, CNC_y, CNC_x+15, CNC_y+15)
+        root.update()
+        time.sleep(0.01)
+
+
+
+
+move(600, 0)
+
+
+#Make the Tkinter window stay open
 root.mainloop()
+
