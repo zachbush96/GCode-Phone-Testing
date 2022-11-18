fc8fb8d11e86162dae6e059685e95de452453abb
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2022-11-18 16:51:14 -0600

    Update ObjectBasedGCode.py

--- ObjectBasedGCode.py
+++ ObjectBasedGCode.py
@@ -8,7 +8,7 @@
 '''
 
 from ast import arg
-from ensurepip import version
+#from ensurepip import version
 import sys
 import serial
 import serial.tools.list_ports
@@ -23,18 +23,26 @@ from Camera import *
 
 
 #FREQUENTLY USED VARIABLES
-TROUBLESHOOTING = False
+TROUBLESHOOTING = True
 BACKOFF = 5 #Hit the limit switch and and then move back a few mm with the force of BACKOFF_FORCE
-BACKOFF_FORCE = 1000
+BACKOFF_FORCE = 3000
 SCREEN_CORDINATES_PADDING = BACKOFF + 4
-FSPEED = 10_000
+FSPEED = 3_000
 COMPORT = ""
+CountofArguments = len(sys.argv)
+Auto = False
+Zero = False
+Name = None
+Port = None
+Image = None
+Camera = None
 
 
 
 #Helper Functions
 def cls():
-  os.system('cls' if os.name=='nt' else 'clear')
+  #os.system('cls' if os.name=='nt' else 'clear')
+  pass
 
 def command(ser, strCommand): #Sends a command to the controller
   
@@ -44,10 +52,11 @@ def command(ser, strCommand): #Sends a command to the controller
     print("Reconnecting to Device")
     ser.close()
     ser1 = serial.Serial(cnc.COMport, 115200, timeout=1) #Reconecting to the device
-    setupConnection(ser1) #Clear the buffer
+    ser = ser1
+    setupConnection(ser) #Clear the buffer
     print("Sleeping for 3 seconds...")
-    time.sleep(3)
-    command(ser1, strCommand) #If the serial port was closed, resend the command now that the port is open
+    time.sleep(10)
+    command(ser, strCommand) #If the serial port was closed, resend the command now that the port is open
   
   ser.write(str.encode(strCommand+"\r\n"))
   response = ser.readline().decode('utf-8') #<--- This is supposed to be the response from the controller, but the response doesnt mean the command was executed until the end
@@ -152,7 +161,7 @@ def set00(ser, cnc): #Goes to the limit switch of each axis, scoots off the limi
     setupConnection(ser) #Setup the new connection
     command(ser, "G91") #Set to relative mode
     command(ser, f'G1 Z300 F{FSPEED}') #Move Z axis Up 
-    time.sleep(5) #Sleep while waiting for large move to complete
+    time.sleep(10) #Sleep while waiting for large move to complete
     command(ser, "$X") #Reset the alarm
     command(ser, "$H")
     ser.close() #Close the connection
@@ -160,7 +169,7 @@ def set00(ser, cnc): #Goes to the limit switch of each axis, scoots off the limi
     time.sleep(1) #Wait for the connection to close
     ser = serial.Serial(cnc.COMport, 115200, timeout=1) #Reopen the connection
     setupConnection(ser) #Setup the new connection
-    command(ser, "$X") #Cleae any alarms
+    command(ser, "$X") #Clear any alarms
     command(ser, "$H")
     time.sleep(1)
     command(ser, f'G1 Z-100 F{FSPEED}') #Move Down until the limit switch releases
@@ -205,10 +214,12 @@ def getUserInput(ser, userInput):
     print("Sending Command")
     command(ser, userInput)
 
-#Emulate a touch on the screen
 def click(ser): #Currently - Lower and then raise the Z-Axis - Future - lower and raise the Z-Axis and also turn on and off the spindle
   command(ser, f'G1 Z-35 F{FSPEED}')
   command(ser, f'G1 Z-30 F{FSPEED}')
+
+
+
 
 
 
@@ -308,13 +319,11 @@ def Receiver(ser, DeviceProfile):
   command(ser, "G1 Y-58") #Move to a touchable location
   click(ser)
 
-def Vibration(ser, DeviceProfile):
+def Vibration(ser, DeviceProfile): #Need a way to check if the vibration motor is working
   print("Starting Vibration Test")
   command(ser, DeviceProfile['testLocations']['Vibration'])
-  command(ser, "G1 Z-25") #Lower the finger
-  command(ser, "G1 Z-15") #Raise the finger
-  command(ser, "G1 Z-25") #Lower the finger
-  command(ser, "G1 Z-15") #Raise the finger
+  click(ser) #Turn on the vibration motor
+  click(ser) #Turn off the vibration motor
 
 def MegaCam(ser, DeviceProfile): #Image is very dark compared to the other images
   print("Starting MegaCam Test")
@@ -343,19 +352,25 @@ def Sensor(ser, DeviceProfile): #Image is very bright compared to the other imag
   command(ser, DeviceProfile['Display']['Back Button'])
   click(ser)
 
-def Speaker(ser, DeviceProfile): 
+def Speaker(ser, DeviceProfile): #Needs some way to test the audio levels that play after the botton is pressed
   print("Starting Speaker Test")
   command(ser, DeviceProfile['testLocations']['Speaker'])
-  click(ser)
-  click(ser)
-  click(ser)
-  click(ser)
+  click(ser) #All Speakers
+  #TestAudioLevels()
+  click(ser) #Bottom Speaker
+  #TestAudioLevels()
+  click(ser) #Top Speaker
+  #TestAudioLevels()
+  click(ser) #Stop the audio
 
 def SubKey(ser, DeviceProfile): 
 
   print("Starting SubKey Test")
   command(ser, DeviceProfile['testLocations']['Sub Key'])
   click(ser)
+  imagePaths = []
+  individualTestResults = []
+  
   command(ser, DeviceProfile['testLocations']['Volume Up'])
   print("Just hit Volume Up")
   command(ser, "G1 Z-25") #Lower the finger
@@ -363,8 +378,9 @@ def SubKey(ser, DeviceProfile):
   command(ser, "G1 Y-131") #Back up off the button
   command(ser, DeviceProfile['testLocations']['Camera Center'])
   print("Just sent Camera Center")
-  time.sleep(10)
-  capturePicture("SubKey_VolumeUp" + str(random.randint(0, 100000)), cnc)
+  time.sleep(20)
+  whiteImage = capturePicture("SubKey_VolumeUp" + str(random.randint(0, 100000)), cnc)
+  imagePaths.append(whiteImage)
 
   command(ser, DeviceProfile['testLocations']['Volume Down'])
   print("Just hit Volume Down")
@@ -373,8 +389,9 @@ def SubKey(ser, DeviceProfile):
   command(ser, "G1 Y-131") #Back up off the button
   command(ser, DeviceProfile['testLocations']['Camera Center'])
   print("Just sent Camera Center")
-  time.sleep(10)
-  capturePicture("SubKey_VolumeDown" + str(random.randint(0, 100000)), cnc)
+  time.sleep(20)
+  greenImage = capturePicture("SubKey_VolumeDown" + str(random.randint(0, 100000)), cnc)
+  imagePaths.append(greenImage)
   
   command(ser, DeviceProfile['testLocations']['Power Button'])
   print("Just hit Power Button")
@@ -383,37 +400,46 @@ def SubKey(ser, DeviceProfile):
   command(ser, "G1 Y-131") #Back up off the button
   command(ser, DeviceProfile['testLocations']['Camera Center'])
   print("Just sent Camera Center")
-  time.sleep(10)
-  capturePicture("SubKey_PowerButton" + str(random.randint(0, 100000)), cnc)
+  time.sleep(20)
+  redImage = capturePicture("SubKey_PowerButton" + str(random.randint(0, 100000)), cnc)
+  imagePaths.append(redImage)
   
   command(ser, DeviceProfile['Display']['Back Button'])
   print("Just hit Back Button")
   click(ser)
   command(ser, DeviceProfile['testLocations']['Camera Center'])
   print("Just sent Camera Center")
-  time.sleep(15)
-  capturePicture("SubKey_BackButton" + str(random.randint(0, 100000)), cnc)
+  time.sleep(20)
+  magentaImage = capturePicture("SubKey_BackButton" + str(random.randint(0, 100000)), cnc)
+  imagePaths.append(magentaImage)
   
   command(ser, DeviceProfile['Display']['Home Button'])
   print("Just hit Home Button")
   click(ser)
   command(ser, DeviceProfile['testLocations']['Camera Center'])
   print("Just sent Camera Center")
-  time.sleep(15)
-  capturePicture("SubKey_HomeButton" + str(random.randint(0, 100000)), cnc)
+  time.sleep(20)
+  blueImage = capturePicture("SubKey_HomeButton" + str(random.randint(0, 100000)), cnc)
+  imagePaths.append(blueImage)
   
   command(ser, DeviceProfile['Display']['Menu Button'])
   print("Just hit Menu Button")
   click(ser)
   command(ser, DeviceProfile['testLocations']['Camera Center'])
   print("Just sent Camera Center")
-  time.sleep(15)
-  capturePicture("SubKey_MenuButton" + str(random.randint(0, 100000)), cnc)
+  time.sleep(20)
+  orangeImage = capturePicture("SubKey_MenuButton" + str(random.randint(0, 100000)), cnc)
+  imagePaths.append(orangeImage)
 
   #Leave the SubKey test (double tap the back button)
   command(ser, DeviceProfile['Display']['Back Button'])
   click(ser)
   click(ser)
+  print("Starting SubKey Image Testing")
+  for imagePath in imagePaths:
+    r = testImages(imagePath) #In the Camera.py file
+    individualTestResults.append(r)
+  print(individualTestResults)
 
 def FrontCamera(ser, DeviceProfile): 
   print("Starting Front Camera Test")
@@ -482,27 +508,39 @@ def Version(ser, DeviceProfile):
   click(ser)
 
 
+
+
+
+
+
+
+
+
+
+
+
 #Once a test is complete, add it to this array to include it in the test suite
 ViableTests = [
-  #Red,
-  #Green,
-  #Blue,
-  Receiver,
-  Vibration,
-  MegaCam,
-  Sensor,
-  screenTest,
-  Speaker,
-  SubKey,
-  FrontCamera,
-  Black,
-  Version
+  #Red, 
+  #Green, 
+  #Blue
+  #Receiver,
+  #Vibration,
+  #MegaCam,
+  #Sensor,
+  #screenTest,
+  #Speaker,
+  SubKey
+  #FrontCamera,
+  #Black,
+  #Version
 ]
 
 
 
 #Device Profile
 GalaxyNote20Ultra = {
+    "IMEI": "000000000000000",
     "X Length": 164.8,
     "Y Length": 77.2,
     "Z Length": 8.1,
@@ -518,7 +556,7 @@ GalaxyNote20Ultra = {
         "Almost Top Right Corner": f'', #Used for the screen test
         "Back Button":f'G1 X80.5 Y-105 F{FSPEED}',
         "Home Button":f'G1 X80.5 Y-88 F{FSPEED}',
-        "Menu Button":f'"G1 X80.5 Y-65 F{FSPEED}',
+        "Menu Button":f'G1 X80.5 Y-65 F{FSPEED}'
     },
     "testLocations": {
         "Red": f'G1 X216 Y-65 F{FSPEED}', #Test Created
@@ -543,7 +581,50 @@ GalaxyNote20Ultra = {
         "Power Button": f'G1 X169 Y-131 F{FSPEED}', #Test Created
         "Volume Down": f'G1 X184 Y-131 F{FSPEED}', #Test Created
         "Volume Up": f'G1 X198 Y-131 F{FSPEED}', #Test Created
-        "Camera Center": f'G1 X153 Y-38 Z5 F{FSPEED}'
+        "Camera Center": f'G1 X153 Y-38 Z5 F{FSPEED}',
+        "Top Speaker": f'UNKNOWN',
+        "Bottom Speaker": f'G1 X75 Y-70 Z-30 F{FSPEED}',
+    },
+    "Test Results": {
+        "Red": "Null",
+        "Green": "Null",
+        "Blue": "Null",
+        "Receiver": "Null",
+        "Vibration": "Null",
+        "Mega Cam": "Null",
+        "Sensor": "Null",
+        "Touch": "Null",
+        "Speaker": 0,
+        "Sub Key": {
+            "Volume Up": "Null",
+            "Volume Down": "Null",
+            "Power": "Null",
+            "Back": "Null",
+            "Home": "Null",
+            "Menu": "Null"
+        },
+        "Front Cam": "Null",
+        "Black": "Null",
+        "Hall IC": "Null",
+        "S-Pen": "Null",
+        "MST Test": {
+            "One Time Track One": "Null",
+            "One Time Track Two": "Null",
+            "Continous Track One": "Null",
+            "Continous Track Two": "Null",
+            "Continous Track One and Two": "Null",
+            "Continous Off": "Null"
+        },
+        "MLC": "Null",
+        "Loopback": {
+            "RCV 1st Mic": "Null",
+            "SPK 2nd Mic": "Null",
+            "SPK 3rd Mic": "Null",
+            "EP": "Null",
+            "Exit Button": "Null"
+        },
+        "Version": "Null",
+        "Grip Sensor": "Null"
     }  
 }
 
@@ -612,13 +693,7 @@ def setupCNC(**options):
 #cls() #Clear the Screen
 
 #Handle passing arguments from the command line
-CountofArguments = len(sys.argv)
-Auto = False
-Zero = False
-Name = None
-Port = None
-Image = None
-Camera = None
+
 if CountofArguments > 1:
   for i in range(1, CountofArguments):
     if sys.argv[i] == "--Name":
@@ -646,6 +721,10 @@ if Auto and Zero:
     command(ser, "G90")
     set00(ser, cnc)
     print("Starting all tests ---az---")
+    ser.close() #Close the serial port that was opened on line 619
+    ser = serial.Serial(cnc.getComPort(), 115200, timeout=1) #Setup the serial port
+    setupConnection(ser)
+    command(ser, "G90")
     for x in ViableTests:
       x(ser, GalaxyNote20Ultra)
     print("All Tests Completed - Going to Home")
@@ -665,79 +744,3 @@ elif Auto and not Zero:
 
 elif Zero and not Auto:
   set00(ser, cnc)
-
-
-
-
-
-
-
-
-
-'''
-if len(sys.argv) < 2:  #Run setup if no arguments are passed
-    cnc = setupCNC()
-    ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-    setupConnection(ser)
-    command(ser, "G90")
-else:
-  if sys.argv[1] == "help":
-    print("Options: COMPORT, CameraID, PATH, Name \{auto\}")
-    sys.exit()
-  if len(sys.argv) == 2:
-      if help in sys.argv:
-          print("Options: COMPORT, CameraID, PATH, Name \{auto\}")
-      else:
-          cnc = setupCNC(COMPORT=sys.argv[1])
-          ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-          setupConnection(ser)
-          command(ser, "G90") #Set the position to absolute mode
-  elif len(sys.argv) == 3:
-      cnc = setupCNC(COMPORT=sys.argv[1], CameraID=sys.argv[2])
-      ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-      setupConnection(ser)
-      command(ser, "G90") #Set the position to absolute mode
-  elif len(sys.argv) == 4:
-      cnc = setupCNC(COMPORT=sys.argv[1], CameraID=sys.argv[2], PATH=sys.argv[3])
-      ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-      setupConnection(ser)
-      command(ser, "G90") #Set the position to absolute mode
-  elif len(sys.argv) == 5:
-      cnc = setupCNC(COMPORT=sys.argv[1], CameraID=sys.argv[2], PATH=sys.argv[3], Name=sys.argv[4])
-      ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-      setupConnection(ser)
-      command(ser, "G90") #Set the position to absolute mode
-  elif len(sys.argv) == 6:
-      if "auto" in sys.argv:
-          cnc = setupCNC(COMPORT=sys.argv[1], CameraID=sys.argv[2], PATH=sys.argv[3], Name=sys.argv[4])
-          ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-          setupConnection(ser)
-          command(ser, "G90") #Set the position to absolute mode
-          #Run all the tests
-          for x in ViableTests:
-            x(ser, GalaxyNote20Ultra)
-          print("Returning to 000, auto")
-          command(ser, "G1 X0 Y0 Z0 F1000") #Return to 0,0,0 to swap out the phone
-          print("All Tests Complete")
-      
-      elif "zero" in sys.argv:
-          cnc = setupCNC(COMPORT=sys.argv[1], CameraID=sys.argv[2], PATH=sys.argv[3], Name=sys.argv[4])
-          ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-          setupConnection(ser)
-          command(ser, "G90")
-          set00(ser, cnc)
-      elif "autozero" in sys.argv:
-          cnc = setupCNC(COMPORT=sys.argv[1], CameraID=sys.argv[2], PATH=sys.argv[3], Name=sys.argv[4])
-          ser = serial.Serial(cnc.COMport, 115200, timeout=1)
-          setupConnection(ser)
-          command(ser, "G90")
-          set00(ser, cnc)
-          for x in ViableTests:
-            x(ser, GalaxyNote20Ultra)
-          print("Going back to 000, autozero")
-          command(ser, "G1 X0 Y0 Z0 F1000") #Return to 0,0,0 to swap out the phone
-          print("All Tests Complete")
-      else:
-          print("Invalid Arguments")
-
-'''
